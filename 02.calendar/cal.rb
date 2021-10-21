6d395a16359ac1795013a31f6324acfe30d03fa3
jinroq <2787780+jinroq@users.noreply.github.com>
2021-10-21 11:13:30 +0900

    レビューの指摘内容を反映
    * 定数宣言時に .freeze をするようにした
    * @current -> @target にリネーム
    * 土曜日の折返し部分を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -6,27 +6,26 @@ require 'optparse'
 
 # Cal クラス
 class Cal
-  SUNDAY_ORIGIN_WEEKS = %w[日 月 火 水 木 金 土]
+  SUNDAY_ORIGIN_WEEKS = %w[日 月 火 水 木 金 土].freeze
 
   def initialize
-    SUNDAY_ORIGIN_WEEKS.freeze
     params = OptionParser.new.getopts('m:y:')
 
     today = Date.today
     if params['m'] && params['y']
-      @current = Date.new(params['y'].to_i, params['m'].to_i, 1)
+      @target = Date.new(params['y'].to_i, params['m'].to_i, 1)
     elsif params['m']
-      @current = Date.new(today.year, params['m'].to_i, 1)
+      @target = Date.new(today.year, params['m'].to_i, 1)
     else
-      @current = today
+      @target = today
     end
   end
 
   def run
     puts_header
 
-    current_month_first_day = Date.new(@current.year, @current.month, 1)
-    current_month_last_day = Date.new(@current.year, @current.month, -1)
+    current_month_first_day = Date.new(@target.year, @target.month, 1)
+    current_month_last_day = Date.new(@target.year, @target.month, -1)
 
     last_day = current_month_last_day.day
     weeks = []
@@ -37,6 +36,7 @@ class Cal
           next
         end
 
+        #weeks.push('  ' * current_month_first_day.wday)
         weeks.push('  ')
         if current_month_first_day.monday?
           weeks.push(current_month_first_day.strftime('%e'))
@@ -72,14 +72,10 @@ class Cal
         weeks.push(current_month_first_day.strftime('%e'))
         puts weeks.join(' ')
         weeks = []
-      elsif (day + 1) == last_day
-        current_day = current_month_first_day + day
-        weeks.push(current_day.strftime('%e'))
-        puts weeks.join(' ')
       else
         current_day = current_month_first_day + day
         weeks.push(current_day.strftime('%e'))
-        if current_day.saturday?
+        if current_day.saturday? || (day + 1) == last_day
           puts weeks.join(' ')
           weeks = []
         end
@@ -90,7 +86,7 @@ class Cal
   private
 
   def puts_header
-    puts "#{' ' * 6}#{@current.month}月 #{@current.year}"
+    puts "#{' ' * 6}#{@target.month}月 #{@target.year}"
     weeks_header = SUNDAY_ORIGIN_WEEKS.join(' ')
     puts weeks_header.to_s
   end
