45942bb5f418dd669ac6a9a8822a1b3ce6e63c1b
jinroq <2787780+jinroq@users.noreply.github.com>
2021-11-04 17:15:03 +0900

    プラクティス「lsコマンドを作る4」
    * `-l` オプションを追加

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -11,10 +11,12 @@ class Ls
 
     opt.on('-a') { |v| params[:a] = v }
     opt.on('-r') { |v| params[:r] = v }
+    opt.on('-l') { |v| params[:l] = v }
     opt.parse!(ARGV)
 
     @files = params[:a] ? Dir.glob('*', File::FNM_DOTMATCH) : Dir.glob('*')
     @files = @files.reverse if params[:r]
+    @list_flag = params[:l]
   end
 
   def run
@@ -28,19 +30,29 @@ class Ls
     files = @files
     size = files.size
 
-    lines = Array.new(size / columns) { [] }
-    max_line = lines.length
+    lines = @list_flag ? [] : Array.new(size / columns) { [] }
+    if @list_flag
+      files.each do |file|
+        lines.push(file)
+      end
+    else
+      max_line = lines.length
 
-    files.each_with_index do |file, i|
-      lines[i % max_line].push(file)
+      files.each_with_index do |file, i|
+        lines[i % max_line].push(file)
+      end
     end
 
     lines
   end
 
   def puts_list(lines)
-    lines.each do |l|
-      puts l.join('	')
+    if @list_flag
+      puts lines
+    else
+      lines.each do |l|
+        puts l.join('	')
+      end
     end
   end
 end
